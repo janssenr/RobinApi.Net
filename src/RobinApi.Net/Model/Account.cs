6b86ba9cbc9c192f246f6b08169dc9e40ed724eb
Rob Janssen <rob.janssen@upcmail.nl>
2017-05-01 19:19:22 +0200

    Account

---
+++ src/RobinApi.Net/Model/Account.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace RobinApi.Net.Model
+{
+    [DataContract]
+    public class Account
+    {
+        [DataMember(Name = "id", EmitDefaultValue = false)]
+        public int Id { get; set; }
+
+        [DataMember(Name = "is_organization", EmitDefaultValue = false)]
+        public bool IsOrganization { get; set; }
+
+        [DataMember(Name = "name", EmitDefaultValue = false)]
+        public string Name { get; set; }
+
+        [DataMember(Name = "slug", EmitDefaultValue = false)]
+        public string Slug { get; set; }
+
+        [DataMember(Name = "avatar", EmitDefaultValue = false)]
+        public string Avatar { get; set; }
+
+        [DataMember(Name = "created_at", EmitDefaultValue = false)]
+        public DateTime CreatedAt { get; set; }
+
+        [DataMember(Name = "updated_at", EmitDefaultValue = false)]
+        public DateTime UpdatedAt { get; set; }
+    }
+}
